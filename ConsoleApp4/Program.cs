6151d3acb099ded6f83b5d3ba4b3f69bda9edee5
dalnik61327@gmail.com <dalnik61327@gmail.com>
2019-12-04 14:15:00 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Деление двух чисел");
+
+            Console.Write("Введите первое число. M-> ");
+            int M = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите второе число. N-> ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            
+            
+
+           
+
+            if (M % N == 0) Console.WriteLine(M / N);
+            else Console.WriteLine("Не делиться нацело");
+            Console.ReadLine();
+        }
+    }
+}
