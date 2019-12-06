d57afe05f42c5517474f31a4a27a450ee2ac6a13
dalnik61327@gmail.com <dalnik61327@gmail.com>
2019-12-06 09:32:18 +0200

    как организова проверку нуля

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -10,18 +10,30 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Деление двух чисел");
-
+        
             Console.Write("Введите первое число. M-> ");
             int M = Convert.ToInt32(Console.ReadLine());
+           
 
             Console.Write("Введите второе число. N-> ");
+
+
             int N = Convert.ToInt32(Console.ReadLine());
 
             
-            
+            //if (N == 0) Console.WriteLine("Число N не может быть равным нулю");
 
-           
+            //     Console.ReadKey();
+            
+            // не могу понять как реализовать проверку делителя на ноль
+
+
+
+
+
+
 
             if (M % N == 0) Console.WriteLine(M / N);
             else Console.WriteLine("Не делиться нацело");
